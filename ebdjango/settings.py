54f824c5b4e43f92ca59f97e1ad4e5f80060e922
sudo <56063116+mkZH0740@users.noreply.github.com>
2023-10-10 15:24:45 -0400

    add allowed host

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z0s)53u=dt0a0ka290vabfn46hy61*^)dfzmjuxc*&@a(4(3fd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.eba-q3i5qbjs.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
